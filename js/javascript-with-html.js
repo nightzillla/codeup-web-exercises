dfa3c536406dfe4b4b887094059733d392aa3e52
nightzillla <hnguyen9833@gmail.com>
2022-08-12 12:22:11 -0500

    Javascript exercise

--- js/javascript-with-html.js
+++ js/javascript-with-html.js
@@ -1,2 +1,11 @@
 // alert("hello world");
-confirm("are you sure you dont want to buy stuff.");
+// confirm("are you sure you dont want to buy stuff.");
+
+// if (userChoice) {
+//     alert("Yay!");
+// } else {
+//     alert("Boo");
+// }
+
+let name = prompt("What's your name?");
+console.log(name);
