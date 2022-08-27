81b173cc4a14f45433f4f3d82eef2db97f8c6691
nightzillla <hnguyen9833@gmail.com>
2022-08-27 17:34:49 -0500

    Working on functions fundamentals

---
+++ js/javascript-101-exercise.js
@@ -0,0 +1,41 @@
+//Array exercise and functions
+
+// Add the string "tomato" onto the end of the vegetables array.
+// *use .push
+// vegetables.push('tomato')
+//
+// assert(
+//     vegetables,
+//     ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini", "tomato"],
+// );
+// *********************************************************************************************************************
+// Writing functions
+// *********************************************************************************************************************
+// This function generates a random number that is both positive and even
+// function randomPositiveEvenNumber() {
+//     let randomNumber = Math.ceil(Math.random() * 100) + 10;
+//     if (randomNumber % 2 !== 0){
+//         return randomPositiveEvenNumber()
+//     }
+//     return randomNumber;
+// }
+// *********************************************************************************************************************
+// This function generates a random number that is both negative and even.
+// function randomNegativeEvenNumber() {
+//     let randomNumber = Math.ceil(Math.random() * -100) -10);
+//     if (randomNumber % 2 === 0) {
+//         return randomNumber;
+//     }
+//     return randomNegativeEvenNumber();
+// }
+// *********************************************************************************************************************
+// Write a function definition named sumOfSquares that takes in two numbers, squares each number, then returns the sum of both squares.
+// function sumOfSquares(a,b){
+//     return ((a*a)+(b*b));
+// }
+// *********************************************************************************************************************
+// Write a function definition named remainder that takes in two numbers and returns
+
+
+
+// *********************************************************************************************************************
