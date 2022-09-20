63486ff73a0e299c80e7bb793ec13c70983f8fce
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-09-20 15:46:02 -0500

    working on jquery examples

---
+++ js/intro-to-jquery.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//     $("h3").click(function(){  // to call jquery type "jQuery" || "$" shorthand for jQuery
+//         $(this).next().slideToggle(500);
+//     });
+// });
+
+function init() {
+    $("h3").click(function(){  // to call jquery type "jQuery" || "$" shorthand for jQuery
+        $(this).next().slideToggle(500);
+    });
+}
+
+window.onload =init;
