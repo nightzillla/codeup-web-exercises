7d725ded1c371782e26eaeae3472b4e3059b8437
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-09-29 17:24:07 -0500

    working on jquery

--- js/jquery-practice.js
+++ js/jquery-practice.js
@@ -3,7 +3,7 @@
 // # is for id
 // . is for class
 // h1 make it red
-// document.getElementsByTagName("h1").
+// document.getElementsByTagName("h1") targets all elements ex h1,div,id
 $(function(){
     let myName = 'Danny';
     $("h1").addClass("text-danger"); //addClass means to do something
@@ -15,11 +15,14 @@ $(function(){
     $("ul li:nth-child(4)").addClass("text-warning");
     // THIS IS A CLASS . .on("click") is a event listener
     $(".fs-4").click(function(){
-        $(this).toggleClass("fs-4");
+        $(this).css("visibility", "hidden");
         // $(".fs-4").toggleClass("fs.4"); // this will toggle both fs-4
     });
     // This target the second ul
     $("ul").last().children().addClass("text-info")
+    $("ul li:nth-child(1)").click(function (){
+        alert("you click first child!");
+    });
 });
 
 // same as  $("#myFavoriteDiv").append(`<h1>Underneath the List ${myName}</h1>
