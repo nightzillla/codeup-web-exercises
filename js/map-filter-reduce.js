0c00e40ccf042983e4aa31eda488930aa9bfa2e2
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-09-30 20:33:34 -0500

    starting map filter

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -1,5 +1,5 @@
-const prices = [32.99, 21.99, 6.99, 4.99, 12.99, 8.98, 5.99];
-let output = '';
+// const prices = [32.99, 21.99, 6.99, 4.99, 12.99, 8.98, 5.99];
+// let output = '';
 
 // prices.forEach((price, index,array) => {
 //
@@ -12,91 +12,130 @@ let output = '';
 //     }
 // });
 
-const priceAfterTax = prices.map(price => {
-    const tax =(price * 0.0825).toFixed(2);
-    const total = (price + parseFloat(tax)).toFixed(2);
-    return parseFloat(total);
-});
-console.log(prices);
-console.log(priceAfterTax);
+// const priceAfterTax = prices.map(price => {
+//     const tax =(price * 0.0825).toFixed(2);
+//     const total = (price + parseFloat(tax)).toFixed(2);
+//     return parseFloat(total);
+// });
+// console.log(prices);
+// console.log(priceAfterTax);
+//
+// // const doubleArray = prices.map(price => price * 2);
+// // console.log(doubleArray);
+//
+// const desserts = ['sherbet', 'whisky cake', 'cupcake', 'eclair'];
+//
+// // const eatingDessert = desserts.map(dessert => ` Eating ${dessert} Yummm!!`);
+// // eatingDessert.forEach(element => {
+// //     $("div").append(`<p>${element}</p>`);
+// // })
+//     // ARRAY OF OBJECTS
+// const cars = [
+//     {
+//         make: "Honda",
+//         model: "Civic",
+//         mileage: 10428
+//     },
+//     {
+//         make: "Toyota",
+//         model: "Corolla",
+//         mileage: 9425
+//     },
+//     {
+//         make: "Ford",
+//         model: "Mustang",
+//         mileage: 2567
+//     },
+//     {
+//         make: "Audi",
+//         model: "A3",
+//         mileage: 14500
+//     },
+//     {
+//         make: "Mazda",
+//         model: "3",
+//         mileage: 11248
+//     }
+// ];
+//
+// const mileages = cars.map(car => car.mileage);
+// console.log(mileages)
+//
+// const newObject = cars.map(car => {
+//     car.newProp = 'new property';
+//     return car;
+// });
+// console.log(cars[0]);
+//
+// const array = [4,5,7,9];
+//
+// function doubleArray(array){
+//     let newArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//         newArray.push(array[i] * 2);
+//     }
+//     return newArray;
+// }
+// console.log(doubleArray)
+//
+// const doubleArray2 = prices.map(price => price * 2);
+// console.log(doubleArray2);
+//
+// const under10k = cars.filter(car => car.mileage < 10000);
+// // we are using object deconstruction
+// under10k.forEach(({make,model, mileage}) => {
+//     output += `<p> I found a ${make} ${model} with ${mileage} miles</p>`;
+//     $("#output").html(output);
+// });
+//
+// prices.filter(price => price < 10)
+//     .map(price => {
+//     const tax =(price * 0.0825).toFixed(2);
+//     const total = (price + parseFloat(tax)).toFixed(2);
+//     return parseFloat(total);
+//     }).forEach(price => $("div").append(`<p>${price}</p>`));
+//                                         // accumulator, element,
+// const totalCost = prices.reduce(function (total,price){
+//     return total + price;
+// });
+// console.log(totalCost);
+// console.log(totalCost);
 
-// const doubleArray = prices.map(price => price * 2);
-// console.log(doubleArray);
 
-const desserts = ['sherbet', 'whisky cake', 'cupcake', 'eclair'];
-
-// const eatingDessert = desserts.map(dessert => ` Eating ${dessert} Yummm!!`);
-// eatingDessert.forEach(element => {
-//     $("div").append(`<p>${element}</p>`);
-// })
-    // ARRAY OF OBJECTS
-const cars = [
+const users = [
     {
-        make: "Honda",
-        model: "Civic",
-        mileage: 10428
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
     },
     {
-        make: "Toyota",
-        model: "Corolla",
-        mileage: 9425
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
     },
     {
-        make: "Ford",
-        model: "Mustang",
-        mileage: 2567
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
     },
     {
-        make: "Audi",
-        model: "A3",
-        mileage: 14500
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
     },
     {
-        make: "Mazda",
-        model: "3",
-        mileage: 11248
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
     }
 ];
-
-const mileages = cars.map(car => car.mileage);
-console.log(mileages)
-
-const newObject = cars.map(car => {
-    car.newProp = 'new property';
-    return car;
-});
-console.log(cars[0]);
-
-const array = [4,5,7,9];
-
-function doubleArray(array){
-    let newArray = [];
-    for (let i = 0; i < array.length; i++) {
-        newArray.push(array[i] * 2);
-    }
-    return newArray;
-}
-console.log(doubleArray)
-
-const doubleArray2 = prices.map(price => price * 2);
-console.log(doubleArray2);
-
-const under10k = cars.filter(car => car.mileage < 10000);
-// we are using object deconstruction
-under10k.forEach(({make,model, mileage}) => {
-    output += `<p> I found a ${make} ${model} with ${mileage} miles</p>`;
-    $("#output").html(output);
-});
-
-prices.filter(price => price < 10)
-    .map(price => {
-    const tax =(price * 0.0825).toFixed(2);
-    const total = (price + parseFloat(tax)).toFixed(2);
-    return parseFloat(total);
-    }).forEach(price => $("div").append(`<p>${price}</p>`));
-                                        // accumulator, element,
-const totalCost = prices.reduce(function (total,price){
-    return total + price;
-});
-console.log(totalCost);
-console.log(totalCost);
