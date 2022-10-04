0b953071c78d83cbca5cf0cd6dbd24968fcf4f7a
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-04 15:57:22 -0500

    finshed resful lecture

--- js/restful-api-lecture.js
+++ js/restful-api-lecture.js
@@ -4,3 +4,95 @@
 // REST means representational state transfer.
 // A pattern of design principles for APIs
 // REST APIs communicate via HTTP request
+
+// In a REST API, communication via HTTP request is used to perform the sandard set of actions called CRUD
+// Create, Read, Update, Delete
+
+// JSON Javascript Object Notation
+// is not mandatory but is popular because it is both human and machine-readable
+
+// https://glitch.com/amused-typical-skunk
+
+// The R in CRUD: READ
+const booksURL = "https://amused-typical-skunk.glitch.me/books";
+const movieURL = "https://amused-typical-skunk.glitch.me/movies";
+function getMovies() {
+    fetch("https://amused-typical-skunk.glitch.me/movies")
+        .then(resp => resp.json()).then(data=>console.log(data));
+}
+
+getMovies();
+
+// The C in CRUD: CREATE
+
+const bookToPost = {
+    title: "Eleanor of Aquitaine",
+    author: {
+        firstName: "Ralph",
+        lastName: "Turner"
+    }
+}
+
+const postOptions = {
+    method: 'POST',
+    headers: {
+        'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify(bookToPost)
+}
+
+function getBooks() {
+    fetch(booksURL).then(resp => resp.json()).then(data=>console.log(data));
+}
+
+getBooks();
+
+// fetch(booksURL, postOptions).then(getBooks);
+
+// The U in CRUD: Updating with PUT and PATCH request
+// We'll use PUT to replace the entire content
+// We'll use PATCH to modify only part fo the entry
+
+let modification = {
+    title: "Eleanor of Aquitaine: Queen of France, Queen of England"
+}
+
+const patchOptions = {
+    method: 'PATCH',
+    headers: {
+        'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify(modification)
+}
+// This select "id": 4
+// THis will modify
+// fetch(booksURL + "/4", patchOptions).then(getBooks);
+
+modification = {
+    title: "Eleanor of Aquitaine and the Four Kings",
+    author: {
+        firstName: "Amy",
+        lastName: "Kelly"
+    }
+}
+
+const putOptions = {
+    method: 'PUT',
+    headers: {
+        'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify(modification)
+}
+// This will modify
+fetch(booksURL + "/1", putOptions).then(getBooks);
+
+// THE D IN CRUD -- Delete
+
+const deleteOptions = {
+    method: 'DELETE',
+    headers: {
+        'Content-Type' : 'application/json'
+    }
+}
+// This will modify
+// fetch(booksURL + "/3", deleteOptions).then(getBooks);
