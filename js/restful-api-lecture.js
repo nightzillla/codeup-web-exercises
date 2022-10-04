67403d2504ea7ec742a72c9af003286dfd0b1b8f
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-04 14:54:16 -0500

    starting rest lecture

---
+++ js/restful-api-lecture.js
@@ -0,0 +1,6 @@
+// API - Application Programming Interface
+// a set of rules defining how applications and/or devices can connect to each other and communicate with one another.
+
+// REST means representational state transfer.
+// A pattern of design principles for APIs
+// REST APIs communicate via HTTP request
